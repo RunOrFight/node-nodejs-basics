af01335219ddd1e9fca89934f79a7308930477b2
RunOrFight <dragoinfury@gmail.com>
2022-12-03 14:49:07 +0300

    [feat]: completed copy opearation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  const rootPath = "./src/fs/";
+  const filesFolderPath = rootPath + "files";
+  const filesCopyFolderPath = rootPath + "files_copy";
+  const operationError = new Error("FS operation failed");
+
+  fs.access(filesFolderPath, (existenceError) => {
+    try {
+      if (existenceError) {
+        throw operationError;
+      }
+      fs.access(filesCopyFolderPath, (existenceError) => {
+        if (!existenceError) {
+          throw operationError;
+        }
+        fs.cp(filesFolderPath, filesCopyFolderPath, { recursive: true }, () => {
+          console.log("FS operation completed");
+        });
+      });
+    } catch (processError) {
+      console.error(processError);
+    }
+  });
 };
 
 copy();
