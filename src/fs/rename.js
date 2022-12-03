2d4c361ed2ae3fd91bd3ae7ea15782cc8ec4b8c1
RunOrFight <dragoinfury@gmail.com>
2022-12-03 15:00:14 +0300

    [feat]: completed rename operation

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from "fs";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const rootPath = "./src/fs/files/";
+  const wrongFilenamePath = rootPath + "wrongFilename.txt";
+  const properFilenamePath = rootPath + "properFilename.md";
+  const operationError = new Error("FS operation failed");
+
+  fs.access(wrongFilenamePath, fs.constants.F_OK, (existenceError) => {
+    try {
+      if (existenceError) {
+        throw operationError;
+      }
+      fs.access(properFilenamePath, fs.constants.F_OK, (existenceError) => {
+        if (!existenceError) {
+          throw operationError;
+        }
+        fs.rename(wrongFilenamePath, properFilenamePath, () => {
+          console.log("FS operation completed");
+        });
+      });
+    } catch (processError) {
+      console.error(processError);
+    }
+  });
 };
 
 await rename();
